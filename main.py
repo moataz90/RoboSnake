bd9f57026d51365956efacf3bd9617f0f6cf22c0
gemaide <m.gschwindt@tum.de>
2017-07-21 12:04:22 +0200

    Add new main function
    added controlling without xbox controller, other bugfixes

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
  text file """
 
 import time
-import math
 
 # using pygame python library (pygame.org)
 import pygame
@@ -33,7 +32,7 @@ def get_command_params(gait):
     elif gait == 'FastForward':
         amplitude_vertical = 15
         amplitude_horizontal = 0
-        frequency = 10
+        frequency = 9
         phase_vertical = 120
         phase_horizontal = 60
     else:
@@ -54,8 +53,13 @@ def main():
     # pygame initialization stuff
     pygame.init()
     pygame.joystick.init()
-    controller = pygame.joystick.Joystick(0)
-    controller.init()
+    # check for controller. if no controller found, switch to mouse control
+    try:
+        controller = pygame.joystick.Joystick(0)
+        controller.init()
+        mode = "controller"
+    except:
+        mode = "mouse"
 
     # initialize angles
     angles = [0 for i in range(num_joints)]
@@ -68,8 +72,8 @@ def main():
     done = False
     gait = ''
     while not done:
-
-        if pygame.event.get():
+        if mode == "controller":
+            pygame.event.get()
             hat = controller.get_hat(0)
             # possible gaits: Forward, Roll
             if hat == (0, 1):
@@ -83,24 +87,37 @@ def main():
             # quit, if x is pressed
             if controller.get_button(0) == 1:
                 done = True
+        elif mode == "mouse":
+            command_file = open('CurrentCommand.txt', 'r')
+            gait = command_file.read()
+            command_file.close()
+        else:
+            print("Error, mode not found.")
+            done = True
 
-            gait_params = get_command_params(gait)
+        gait_params = get_command_params(gait)
 
-            # get the update for old joint angles
-            t = time.clock()-start_time
-            snake = SnakeSubstitute(num_joints)
-            absolute_angles = snake.get_angles_abs(gait_params[0], gait_params[1], gait_params[2], gait_params[3], gait_params[4], t)
+        # get the update for old joint angles
+        t = time.clock()-start_time
+        snake = SnakeSubstitute(num_joints)
+        absolute_angles = snake.get_angles_abs(gait_params[0], gait_params[1], gait_params[2], gait_params[3], gait_params[4], t)
 
-            # round angles
-            for j in range(0, num_joints):
-                angles[j] = round(absolute_angles[j])
+        # round angles
+        for j in range(0, num_joints):
+            angles[j] = round(absolute_angles[j])
 
-            # update joint angles file
+        # update joint angles file
+        try:
             angle_file = open('joint_angles.txt','w')
             for k in range(0, num_joints-1):
                 angle_file.write(str(k)+":"+str(angles[k]*10)+"&")
             angle_file.write(str(num_joints-1)+":"+str(angles[num_joints-1]*10))
             angle_file.close()
+        except:
+            pass
+
+        # short time delay for smoothness of motion
+        time.sleep(0.01)
 
     pygame.quit()
 
